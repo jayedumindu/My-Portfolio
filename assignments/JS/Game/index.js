6d7468506e834ce01c3da7a554ce9f3778a94673
jayedumindu <jayedumindu@gamil.com>
2022-11-28 21:33:43 +0530

    level 3  functionality added

--- assignments/JS/Game/index.js
+++ assignments/JS/Game/index.js
@@ -56,13 +56,15 @@ let deadURLs = [
 ];
 
 let runningFrames = [];
+let idleFrameIds = [];
 
 function makeSpriteRun() {
   // run infinitely
   let startTime = 500;
   runningImgURLs.forEach((url) => {
     let id = setInterval(function () {
-      if (terminateAll) {
+      if (terminateAll | jumping) {
+        return;
       }
       $(".character").attr("src", urlPrefix + url);
       // console.log("character changed!")
@@ -82,22 +84,23 @@ function makeSpriteJump() {
   }, 0);
   setTimeout(() => {
     $(".character").css({
-      top: "50",
-    });
-    rectSelection = selection.getBoundingClientRect();
-  }, 250);
-  setTimeout(() => {
-    $(".character").css({
       top: "62%",
     });
     rectSelection = selection.getBoundingClientRect();
-  }, 700);
+  }, 750);
 }
 
 document.body.onkeyup = function (e) {
   if ((e.key == " " || e.code == "Space" || e.keyCode == 32) & !terminateAll) {
+    e.preventDefault();
+    jumping = true;
+    // play audio
+    new Audio("./audio/character-jumping.wav").play();
     // make sprite jump
     makeSpriteJump();
+    setTimeout(() => {
+      jumping = false;
+    }, 500);
   }
 };
 
@@ -108,13 +111,23 @@ var getElements = function (selector) {
   return $(selector);
 };
 
+// randomGenerator
+let generator = {
+  prefix: 500,
+  postfix: 2000,
+  generate: function () {
+    return Math.floor(Math.random() * this.prefix) + this.postfix;
+  },
+};
+
+var pixelDepth = 2;
 // generate slimes randomly
 function createSlimesRepeatedly() {
   if (terminateAll) {
     return;
   }
   //  random num between 500 - 1000
-  let randomSeq = Math.floor(Math.random() * 500) + 2000;
+  let randomSeq = generator.generate();
   setTimeout(() => {
     // first append the slime
     let img = $("<img>", {
@@ -126,7 +139,7 @@ function createSlimesRepeatedly() {
     //  then animate
     let intervalId = setInterval(function () {
       let prevPos = $(elem).css("left").match(/(\d+)/)[0];
-      let next = parseInt(prevPos) + 2;
+      let next = parseInt(prevPos) + pixelDepth;
       $(elem).css("left", next + "px");
     }, 10);
     let elem = document.getElementById(slimeId);
@@ -138,6 +151,9 @@ function createSlimesRepeatedly() {
       clearInterval(intervalId);
       $(elem).remove();
     }, 5000);
+    // if (!terminateAll & started) {
+    //   setTimeout(changeScore, 5000);
+    // }
     // set the next slime
     setTimeout(() => {
       slimeId++;
@@ -150,7 +166,7 @@ let treeId = 0;
 
 // used for pausing the whole behaviour
 let terminateAll = false;
-
+let jumping = false;
 function putCharacterToDeadMode() {
   // stop running intervals
   runningFrames.forEach((id) => {
@@ -166,46 +182,35 @@ function putCharacterToDeadMode() {
   });
 }
 
-// function createBackgroundAnimation() {
-//   let randomSeq = Math.floor(Math.random() * 2000) + 500;
-//   setTimeout(() => {
-//     // first append the slime
-//     let img = $("<img>", {
-//       class: "background1",
-//       id: treeId,
-//       src: "./backgrounds/tree1.png",
-//     });
-//     $(".main-inner").append(img);
-//     //  then animate
-//     let elem = document.getElementById(treeId);
-//     let intervalId = setInterval(function () {
-//       let prevPos = $(elem).css("left").match(/(\d+)/)[0];
-//       let next = parseInt(prevPos) + 2;
-//       $(elem).css("left", next + "px");
-//     }, 10);
-//     //  set the stopper
-//     setTimeout(function () {
-//       clearInterval(intervalId);
-//       $(elem).remove();
-//     }, 1000);
-//     // set the next slime
-//     setTimeout(() => {
-//       slimeId--;
-//       createBackgroundAnimation();
-//     }, 100);
-//   }, randomSeq);
-// }
+let idleFrames = [
+  // "sneak_01.png",
+  "idle_01.png",
+  "still_01.png",
+  "idle_02.png",
+  // "sneak_02.png",
+];
+function animateCharacterForIdleMode() {
+  let startTime = 200;
+  idleFrames.forEach((url) => {
+    id = setInterval(function () {
+      $(".character").attr("src", urlPrefix + url);
+    }, startTime);
+    idleFrameIds.push(id);
+    startTime += 1000;
+  });
+}
 
-// createBackgroundAnimation();
+// to-do in presentation
+animateCharacterForIdleMode();
+playAudio();
 
-// $(function () {
-//   $(".slime").draggable();
-//   $(".character").droppable({
-//     drop: function (event, ui) {
-//       console.log("oh i hit something!");
-//     },
-//   });
-// });
+var backgroundAudio;
+
+function playAudio() {
+  backgroundAudio = new Audio("./audio/character-running.wav");
+  backgroundAudio.loop = true;
+  backgroundAudio.play();
+}
 
 // collision checker
 var selection = document.querySelector(".character");
@@ -219,19 +224,124 @@ function checkForTheCollision(elem, id) {
     rect.left < rectSelection.right
   ) {
     terminateAll = true;
+    started = false;
+    stopTimer();
+    resetScore();
     console.log("aww i hit something!");
+    // stops background audio
+    backgroundAudio.pause();
+    // play audio
+    new Audio("./audio/character-dead.wav").play();
     putCharacterToDeadMode();
     clearInterval(id);
+    // make start button appear
+    $("#startBtn").fadeIn();
+    $("#closeBtn").fadeOut();
   } else {
     // if global atttr is set pause the game
     if (terminateAll) clearInterval(id);
   }
 }
 
+let reviveFrames;
+var started = false;
 // start button behaviour
 $("#startBtn").click(function (event) {
+  started = true;
+  resetScore();
   $(this).fadeOut();
+  if (backgroundAudio.paused) backgroundAudio.play();
   // start the process
+  //  change background
+  moveBackgroundAnimationId = setInterval(moveBackground, 100);
+  terminateAll = false;
+  $("#closeBtn").fadeIn();
+  // stop idling
+  idleFrameIds.forEach((id) => {
+    clearInterval(id);
+  });
+  startTimer();
+  makeBannerVisible($("#lvl1Banner"));
   makeSpriteRun();
   createSlimesRepeatedly();
 });
+
+// close btn - haults the current process
+$("#closeBtn").fadeOut();
+$("#closeBtn").click(function () {
+  started = false;
+  stopTimer();
+  resetScore();
+  terminateAll = true;
+  $(this).fadeOut();
+  $("#startBtn").fadeIn();
+});
+
+//  countdown for switching levels
+let timer = 0;
+let timerId;
+
+function startTimer() {
+  timerId = setInterval(changerTimer, 1000);
+}
+
+function stopTimer() {
+  if (timerId != null) clearInterval(timerId);
+  timer = 0;
+}
+
+function changerTimer() {
+  timer++;
+  if (timer == 30) {
+    // level 2
+    makeBannerVisible($("#lvl2Banner"));
+    // change the speed
+    pixelDepth = 3;
+  } else if (timer == 60) {
+    // level 3
+    makeBannerVisible($("#lvl3Banner"));
+    //  change pixel-depth
+    generator.postfix = 1000;
+  } else if (timer == 90) {
+    // level 4
+    makeBannerVisible($("lvl4Banner"));
+  }
+  console.log(timer);
+}
+
+//  banners for levels
+function makeBannerVisible(banner) {
+  banner.css("visibility", "visible");
+  banner.fadeIn();
+  setTimeout(() => {
+    banner.fadeOut();
+  }, 1000);
+}
+
+var backgroundImagePositionX = 0;
+var moveBackgroundAnimationId;
+
+//background move Animation function starter
+function moveBackground() {
+  if (!terminateAll) {
+    backgroundImagePositionX = backgroundImagePositionX + 15;
+    $(".main-inner").css(
+      "backgroundPositionX",
+      backgroundImagePositionX + "px"
+    );
+    console.log(backgroundImagePositionX + "px");
+  }
+}
+
+// score
+var score = 0;
+$("#score").html(score);
+function changeScore() {
+  score++;
+  $("#score").html(score);
+}
+
+function resetScore() {
+  score = 0;
+  $("#score").html(score);
+}
